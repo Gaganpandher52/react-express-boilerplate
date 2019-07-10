220ff4de32c107a9789205fbf74fd50f8bf7e2a0
Gaganpandher52 <gaganpandher52@gmail.com>
2019-07-10 23:54:31 +0000

    clean up code

--- react-front-end/src/App.js
+++ react-front-end/src/App.js
@@ -4,82 +4,13 @@ import './App.css';
 import ReactBnbGallery from 'react-bnb-gallery'
 import Gallery from "react-photo-gallery";
 
-// const photos = [{
-//   photo: "https://source.unsplash.com/aZjw7xI3QAA/1144x763",
-//   caption: "Viñales, Pinar del Río, Cuba",
-//   subcaption: "Photo by Simon Matzinger on Unsplash",
-//   thumbnail: "https://source.unsplash.com/aZjw7xI3QAA/100x67",
-// }, {
-//   photo: "https://source.unsplash.com/c77MgFOt7e0/1144x763",
-//   caption: "La Habana, Cuba",
-//   subcaption: "Photo by Gerardo Sanchez on Unsplash",
-//   thumbnail: "https://source.unsplash.com/c77MgFOt7e0/100x67",
-// }, {
-//   photo: "https://source.unsplash.com/QdBHnkBdu4g/1144x763",
-//   caption: "Woman smoking a tobacco",
-//   subcaption: "Photo by Hannah Cauhepe on Unsplash",
-//   thumbnail: "https://source.unsplash.com/QdBHnkBdu4g/100x67",
-// }];
-
-const photos_new = [
-  {
-    src: "https://source.unsplash.com/Dm-qxdynoEc/800x799",
-    width: 1,
-    height: 1
-  },
-  {
-    src: "https://source.unsplash.com/qDkso9nvCg0/600x799",
-    width: 3,
-    height: 4
-  },
-  {
-    src: "https://source.unsplash.com/iecJiKe_RNg/600x799",
-    width: 3,
-    height: 4
-  },
-  {
-    src: "https://source.unsplash.com/epcsn8Ed8kY/600x799",
-    width: 3,
-    height: 4
-  },
-  {
-    src: "https://source.unsplash.com/NQSWvyVRIJk/800x599",
-    width: 4,
-    height: 3
-  },
-  {
-    src: "https://source.unsplash.com/u9cG4cuJ6bU/4927x1000",
-    width: 4927,
-    height: 1000
-  }
-];
-
-let photos_new1 = [{
-  src: "https://source.unsplash.com/u9cG4cuJ6bU/4927x1000",
-  width: 4927,
-  height: 1000
-}]; //empty array of urls
 class App extends Component {
   constructor(props) {
     super(props)
-    // this.state = {
-    //   message: 'Click the button to load data!'
-    // }
     this.state = { 
       imgs: [] };
     this.toggleGallery = this.toggleGallery.bind(this);
   }
-  // componentDidMount() {
-  //   axios
-  //     .get('https://api.unsplash.com/photos/?client_id=' + '26d750fbc6c381bb4b799351cdf5021c7251dcb04251a4956aa84e50234e8637')
-  //     .then(data => {
-  //       this.setState({ imgs: data.data });
-  //       console.log('seems to work!')
-  //     })
-  //     .catch(err => {
-  //       console.log('Error happened during fetching!', err);
-  //     });
-  // }
 
   fetchData = () => {
     console.log(this.imgs);
