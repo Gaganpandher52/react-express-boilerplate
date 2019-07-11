9c66d0d39b51c92f9b17c72e9017b878bf81ebc1
Gaganpandher52 <gaganpandher52@gmail.com>
2019-07-11 03:04:50 +0000

    api key added to .env

--- react-front-end/src/App.js
+++ react-front-end/src/App.js
@@ -3,21 +3,19 @@ import axios from 'axios';
 import './App.css';
 import ReactBnbGallery from 'react-bnb-gallery'
 import Gallery from "react-photo-gallery";
-// import config from '../config/environment';
-const API_KEY =`${process.env.API_KEY}`
-const api_key = process.env.API_KEY;
-console.log(process.env.API_KEY)
+
+console.log(process.env)
 class App extends Component {
   constructor(props) {
     super(props)
     this.state = { 
       imgs: [] };
-    this.toggleGallery = this.toggleGallery.bind(this);
+    
   }
 
   fetchData = () => {
     console.log(this.imgs);
-    axios.get('https://api.unsplash.com/photos/?client_id=' + api_key)
+    axios.get('https://api.unsplash.com/photos/?client_id=' + process.env.REACT_APP_API_KEY)
     .then((response) => {
 
       let images = [];//empty array of images
@@ -25,15 +23,11 @@ class App extends Component {
       for(let urls in response.data){
         images.push({'src':response.data[urls].urls.raw, 'width':200, 'height':150})
       }
+      console.log(images)
         this.setState({
           imgs: images
       });
     }) 
-  }
-  toggleGallery() {
-    this.setState(prevState => ({
-      galleryOpened: !prevState.galleryOpened
-    }));
   }
 
   render () {
